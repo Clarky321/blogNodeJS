ac3bc89bcd84ef24b2307562caba18b72e5a3a3a
LORAN <vinana222@gmail.com>
2024-02-08 15:26:58 +0300

    React-redux/Toolkit

---
+++ client/src/redux/features/auth/authSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    user: null,
+    token: null,
+    isLoading: false,
+    status: null,
+}
+
+export const authSlice = createSlice({
+    name
+})
